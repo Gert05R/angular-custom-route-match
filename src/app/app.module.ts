bbc8f5b5da8fac59c4fcb36fad90c66ff2a4480f
GERT05 <raeymaekers.gert1@gmail.com>
2022-08-18 00:33:55 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+//imported
+import { RouterModule, UrlSegment } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+  declarations: [ AppComponent, ProfileComponent ],
+  imports:      [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    FormsModule,
+    RouterModule.forRoot([{
+      matcher: (url) => {
+        if (url.length === 1 && url[0].path.match(/^@[\w]+$/gm)) {
+          return {
+            consumed: url,
+            posParams: {
+              username: new UrlSegment(url[0].path.slice(1), {})
+            }
+          };
+        }
+
+        return null;
+      },
+      component: ProfileComponent
+    }
+
+    ])],
+  bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
